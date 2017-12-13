2a0d43ea09b2996fb4dd3101d9a8e6a08a848963
Jakub Kaliszuk <33351633+Wetido@users.noreply.github.com>
2017-12-13 19:20:33 +0100

    Dodanie poruszanie się obiektu oraz kamery za obiektem.

---
+++ Cafe Run/Assets/Cameramove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameramove : MonoBehaviour {
+
+    public Transform character;
+    public Vector3 offset;
+
+    void Update()
+    {
+        ////podążanie kamery za postacią
+        transform.position = character.position + offset;
+    }
+}
---
+++ Cafe Run/Assets/movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float force = 0f;
+
+    void FixedUpdate()
+    {
+
+        rb.AddForce(force * Time.deltaTime, 0, 0);
+
+    }
+
+}
+
